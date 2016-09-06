5caeadd6de5658ecc07a70a9b22f0d902ee4faa0
Аббас Гусенов <gusenov@live.ru>
2016-09-06 20:33:52 +0600

    Moved JSDoc HTML documentation into docs/ directory

--- generate_documentation.bat
+++ generate_documentation.bat
@@ -1,4 +1,4 @@
-set agws_doc_path=%cd%\doc
-@echo %agws_doc_path%
-@RD /S /Q "%agws_doc_path%"
-jsdoc agws.js --destination ./doc/
+set agws_docs_path=%cd%\docs
+@echo %agws_docs_path%
+@RD /S /Q "%agws_docs_path%"
+jsdoc agws.js --destination ./docs/
