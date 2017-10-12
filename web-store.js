a0218652415ada360dd60c51e5ae8313262c24af
Аббас Гусенов <gusenov@live.ru>
2017-10-12 22:21:35 +0600

    0.0.4

--- web-store.js
+++ web-store.js
@@ -385,7 +385,7 @@
             var foundRecordIndex = -1;
             this.forEachRecord(function (record, recordIndex) {
                 if (record.hasOwnProperty(property)
-                        && JSON.stringify(record[property]).match(value)) {
+                        && record[property].match(value)) {
                     foundRecordIndex = recordIndex;
                     return true;
                 }
